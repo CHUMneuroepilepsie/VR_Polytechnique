4d0f908f0617dee0c194f5f39702f369ad2d274d
4rnaudB <arnaudbreheret14@gmail.com>
2022-09-24 12:29:16 -0400

    Ajout de la fonctionnalité anglais/français

--- VR_Polytechnique/Assets/Scripts/MenuManager.cs
+++ VR_Polytechnique/Assets/Scripts/MenuManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void QuitGame()
     {
         Application.Quit();
