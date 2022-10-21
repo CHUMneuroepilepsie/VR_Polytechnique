633e2d704ab1ee10489c6ac18053267177360a9a
4rnaudB <arnaudbreheret14@gmail.com>
2022-10-21 15:33:45 -0400

    Bases des scripts de sauvegarde avec un exemple de la sauvegarde du nombre de fois que le bouton pause a été appuyé

--- VR_Polytechnique/Assets/Scripts/PauseMenu.cs
+++ VR_Polytechnique/Assets/Scripts/PauseMenu.cs
@@ -4,17 +4,28 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour, IDataPersistence
 {
     // Start is called before the first frame update
     // Update is called once per frame
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    private int NbPaused = 0;
 
     private void Start()
     {
         GameIsPaused = false;
         pauseMenuUI.SetActive(false);
+    }
+
+    public void LoadData(GameData data)
+    {
+        this.NbPaused = data.NbPaused; 
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.NbPaused = this.NbPaused;
     }
 
     void Update()
@@ -41,6 +52,7 @@ public class PauseMenu : MonoBehaviour
     }
     void PauseGame()
     {
+        NbPaused++;
         PlayerPrefs.SetInt("TimerPaused", 1);
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
